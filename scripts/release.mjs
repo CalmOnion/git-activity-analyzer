06602eaf55828bd80a9f52b853e3f50b75d9c170
Kristoffer Roen-Lie <kristoffer.roenlie@ad.eyeshare.no>
2024-04-20 14:59:24 +0200

    Made zip release file a bit prettier.

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -6,6 +6,6 @@ execSync('cd ./gaa && dotnet publish --runtime win-x64 -o ../releases/win-x64', 
 execSync('cd ./gaa && dotnet publish --runtime linux-x64 -o ../releases/linux-x64', { stdio: 'inherit' });
 execSync('cd ./gaa && dotnet publish --runtime osx-arm64 -o ../releases/osx-arm64', { stdio: 'inherit' });
 
-execSync('zip -r ./releases/win-x64-gaa.zip ./releases/win-x64', { stdio: 'inherit' });
-execSync('zip -r ./releases/linux-x64-gaa.zip ./releases/linux-x64', { stdio: 'inherit' });
-execSync('zip -r ./releases/osx-arm64-gaa.zip ./releases/osx-arm64', { stdio: 'inherit' });
+execSync('zip ./releases/win-x64-gaa.zip ./releases/win-x64/gaa.exe', { stdio: 'inherit' });
+execSync('zip ./releases/linux-x64-gaa.zip ./releases/linux-x64/gaa', { stdio: 'inherit' });
+execSync('zip ./releases/osx-arm64-gaa.zip ./releases/osx-arm64/gaa', { stdio: 'inherit' });
