2a5c3ed4765b0aab635d4f2765b6566a47b68ebc
Gaute Løken <gautelo@gmail.com>
2024-04-20 16:34:45 +0200

    Added more features to GitQuery. Get authors, fetch latest when accessing the repository, get commits by repository info etc.

--- core/CommitInfo.cs
+++ core/CommitInfo.cs
@@ -3,10 +3,20 @@
 public record CommitInfo
 {
 	public required string Id { get; init; }
-	public required IReadOnlyCollection<Change> Changes{ get; init; }
+	public required string Message { get; init; }
+	public required IReadOnlyCollection<Change> Changes { get; init; }
+	public required DateTimeOffset Date { get; init; }
+	public required Actor Author { get; init; }
 }
 
-public record Change {
+public record Change
+{
 	public required string Path { get; init; }
 	public required string Patch { get; init; }
+}
+
+public record Actor
+{
+	public required string Name { get; init; }
+	public required string Email { get; init; }
 }
--- core/GitQuery.cs
+++ core/GitQuery.cs
@@ -5,49 +5,71 @@ namespace CalmOnion.GAA;
 
 public class GitQuery
 {
+	protected static readonly Regex EmailExpression = new(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.IgnoreCase);
+
+	/// <summary>
+	/// Get authors from the repository.
+	/// </summary>
+	public ISet<Actor> GetRepositoryAuthors(RepositoryInfo repository)
+	{
+		using var repo = GetRepository(repository);
+
+		var authors = new HashSet<Actor>();
+
+		foreach (var commit in repo.Commits)
+		{
+			authors.Add(new Actor
+			{
+				Name = commit.Author.Name,
+				Email = commit.Author.Email
+			});
+		}
+
+		return authors;
+	}
+
 	/// <summary>
 	/// Map information about commits made to a collection of repositories by an Author
 	/// </summary>
-	public IDictionary<string, IReadOnlyCollection<CommitInfo>> MapCommitInfos(DateTimeOffset fromDate, DateTimeOffset toDate, params RepositoryInfo[] infos) {
-		var tasks = infos.Select(info => Task.Factory.StartNew(() => (info, commits: GetCommitInfos(fromDate, toDate, info).ToArray())));
+	public IDictionary<RepositoryInfo, ICollection<CommitInfo>> GetCommitsByRepository(DateTimeOffset fromDate, DateTimeOffset toDate, params RepositoryInfo[] infos)
+	{
+		var tasks = infos.Select(info => Task.Factory.StartNew(() => (info, commits: GetCommits(fromDate, toDate, info).ToArray() as ICollection<CommitInfo>)));
 
 		var results = Task.WhenAll(tasks.ToArray()).Result;
 
-		return results.ToDictionary(
-			t => t.info.Name,
-			t => t.commits as IReadOnlyCollection<CommitInfo>);
+		return results.ToDictionary(t => t.info, t => t.commits);
 	}
 
 	/// <summary>
 	/// Get information about commits made to the repository by the Author
 	/// </summary>
-	public IEnumerable<CommitInfo> GetCommitInfos(DateTimeOffset fromDate, DateTimeOffset toDate, RepositoryInfo repository)
+	public IEnumerable<CommitInfo> GetCommits(DateTimeOffset fromDate, DateTimeOffset toDate, RepositoryInfo repository)
 	{
-		var authorEmail = repository.Author is not null && Regex.IsMatch(repository.Author, @"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.IgnoreCase) ? repository.Author : null;
-		var authorName = authorEmail is null ? repository.Author : null;
+		var authorEmails = new HashSet<string>();
+		var authorNames = new HashSet<string>();
 
-		var tempPath = Path.Combine(Path.GetTempPath(), nameof(CalmOnion), nameof(GAA));
-		var tempRepoPath = Path.Combine(tempPath, repository.Name);
-
-		var cloneOptions = new CloneOptions { Checkout = false };
-		if (repository.Username is not null && repository.Password is not null) {
-			var cred = new UsernamePasswordCredentials { Username = repository.Username, Password = repository.Password };
-			cloneOptions.FetchOptions.CredentialsProvider = (_url, _user, _cred) => cred;
+		if (repository.Authors is not null)
+		{
+			foreach (var author in repository.Authors)
+			{
+				if (EmailExpression.IsMatch(author))
+					authorEmails.Add(author);
+				else
+					authorNames.Add(author);
+			}
 		}
 
-		if (!Directory.Exists(tempRepoPath))
-			Repository.Clone(repository.Url, tempRepoPath, cloneOptions);
-
-		using var repo = new Repository(tempRepoPath);
+		using var repo = GetRepository(repository);
 
 		var commits = repo.Commits
-			.TakeWhile(c => c.Author.When >= fromDate && c.Author.When < toDate)
+			.SkipWhile(c => c.Author.When > toDate)
+			.TakeWhile(c => c.Author.When >= fromDate)
 			.Where(c =>
 			{
 				// Filter out commits by other authors.
-				if (authorEmail is not null && !c.Author.Email.Equals(authorEmail, StringComparison.OrdinalIgnoreCase))
+				if (authorEmails.Count > 0 && !authorEmails.Contains(c.Author.Email, StringComparer.OrdinalIgnoreCase))
 					return false;
-				if (authorName is not null && !c.Author.Name.Equals(authorName, StringComparison.OrdinalIgnoreCase))
+				if (authorNames.Count > 0 && !authorNames.Contains(c.Author.Name, StringComparer.OrdinalIgnoreCase))
 					return false;
 
 				// Filter out merge commits.
@@ -68,9 +90,55 @@ public class GitQuery
 
 			yield return new CommitInfo
 			{
-				Id = commit.Sha,
+				Id = commit.Sha[0..8],
+				Message = commit.Message,
 				Changes = patches.Select(p => new Change { Path = p.Path, Patch = p.Patch }).ToArray(),
+				Date = commit.Author.When,
+				Author = new Actor
+				{
+					Name = commit.Author.Name,
+					Email = commit.Author.Email,
+				},
 			};
 		}
+	}
+
+	protected Repository GetRepository(RepositoryInfo repository, bool fetchLatest = true)
+	{
+		var tempPath = Path.Combine(Path.GetTempPath(), nameof(CalmOnion), nameof(GAA));
+		var tempRepoPath = Path.Combine(tempPath, repository.Name);
+
+		var cloneOptions = new CloneOptions { Checkout = false };
+		if (repository.Username is not null && repository.Password is not null)
+		{
+			var cred = new UsernamePasswordCredentials { Username = repository.Username, Password = repository.Password };
+			cloneOptions.FetchOptions.CredentialsProvider = (_url, _user, _cred) => cred;
+		}
+
+		var existed = Directory.Exists(tempRepoPath);
+		if (!existed)
+			Repository.Clone(repository.Url, tempRepoPath, cloneOptions);
+
+		var repo = new Repository(tempRepoPath);
+
+		if (existed && fetchLatest)
+		{
+			var fetchOptions = new FetchOptions
+			{
+				Prune = true,
+				TagFetchMode = TagFetchMode.Auto,
+			};
+
+			if (repository.Username is not null && repository.Password is not null)
+			{
+				var cred = new UsernamePasswordCredentials { Username = repository.Username, Password = repository.Password };
+				fetchOptions.CredentialsProvider = (_url, _user, _cred) => cred;
+			}
+
+			var refSpecs = $"+refs/heads/*:refs/remotes/origin/*";
+			Commands.Fetch(repo, "origin", [refSpecs], fetchOptions, "Fetching remote");
+		}
+
+		return repo;
 	}
 }
