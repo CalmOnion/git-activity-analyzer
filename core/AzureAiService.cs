ee8edf0f55ab14d45bb94ba5a7ceb13524bd57c7
Kristoffer Roen-Lie <kristoffer.roenlie@ad.eyeshare.no>
2024-04-23 21:47:11 +0200

    Made it possible to decide the explain prompt as well as amount of tokens. Had to change default assignment syntax slightly to make it nicer to consume.

--- core/AzureAiService.cs
+++ core/AzureAiService.cs
@@ -9,13 +9,28 @@ public class AzureAiService(string ApiKey, string ResourceName, string Deploymen
 	private readonly Lazy<OpenAIClient> _client = new(() =>
 	{
 		var endpoint = $"https://{ResourceName}.openai.azure.com/";
-		return new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(ApiKey), new OpenAIClientOptions(OpenAIClientOptions.ServiceVersion.V2024_04_01_Preview));
+
+		return new OpenAIClient(
+			new Uri(endpoint),
+			new AzureKeyCredential(ApiKey),
+			new OpenAIClientOptions(OpenAIClientOptions.ServiceVersion.V2024_04_01_Preview)
+		);
 	});
 
 	public OpenAIClient Client => _client.Value;
 
-	public string? SummarizeCommits(IDictionary<RepositoryInfo, ICollection<CommitInfo>> commitsByRepo, string prompt = SummaryPrompts.Technical)
+	static int GetMaxTokens(int? maxTokens) =>
+		maxTokens is null ? 2048 : Math.Min(maxTokens.Value, 10_000);
+
+	public string? SummarizeCommits(
+		IDictionary<RepositoryInfo, ICollection<CommitInfo>> commitsByRepo,
+		string? prompt = null,
+		int? maxTokens = null
+	)
 	{
+		prompt = string.IsNullOrWhiteSpace(prompt) ? SummaryPrompts.Technical : prompt;
+		maxTokens = GetMaxTokens(maxTokens);
+
 		var sb = new StringBuilder();
 		foreach (var (repo, commits) in commitsByRepo)
 		{
@@ -29,43 +44,60 @@ public class AzureAiService(string ApiKey, string ResourceName, string Deploymen
 			new ChatRequestUserMessage(prompt),
 		])
 		{
-			MaxTokens = 2048,
+			MaxTokens = maxTokens,
 		};
-		var response = Client.GetChatCompletions(completionOptions) ?? throw new Exception("No response from Azure open-AI.");
+		var response = Client.GetChatCompletions(completionOptions)
+			?? throw new Exception("No response from Azure open-AI.");
 
 		var summary = response.Value.Choices[0]?.Message?.Content;
 
 		return summary;
 	}
 
-	public string? ExplainCommit(CommitInfo commitInfo)
+	public string? ExplainCommit(
+		CommitInfo commitInfo,
+		string? prompt = null,
+		int? maxTokens = null
+	)
 	{
-		var sb = new StringBuilder();
-		sb.AppendLine("Consider the following commit message and diffs, explain what I did as if you were me.");
+		prompt = string.IsNullOrWhiteSpace(prompt) ? ExplanationPrompts.AsSelf : prompt;
+		maxTokens = GetMaxTokens(maxTokens);
 
+		var sb = new StringBuilder();
+		sb.AppendLine("Consider the following commit message and diffs.");
 		RenderCommitInfo(sb, commitInfo, true);
 
 		var completionOptions = new ChatCompletionsOptions(DeploymentName, [
 			new ChatRequestSystemMessage("You are a helpful assistant."),
 			new ChatRequestUserMessage(sb.ToString()),
+			new ChatRequestUserMessage(prompt),
 		])
 		{
-			MaxTokens = 2048,
+			MaxTokens = maxTokens,
 		};
-		var response = Client.GetChatCompletions(completionOptions) ?? throw new Exception("No response from Azure open-AI.");
+		var response = Client.GetChatCompletions(completionOptions)
+			?? throw new Exception("No response from Azure open-AI.");
 
 		var explanation = response.Value.Choices[0]?.Message?.Content;
 
 		return explanation;
 	}
 
-	public void RenderCommitInfos(StringBuilder sb, ICollection<CommitInfo> infos, bool includeDiffs = false)
+	public static void RenderCommitInfos(
+		StringBuilder sb,
+		ICollection<CommitInfo> infos,
+		bool includeDiffs = false
+	)
 	{
 		foreach (var info in infos)
 			RenderCommitInfo(sb, info, includeDiffs);
 	}
 
-	private static void RenderCommitInfo(StringBuilder sb, CommitInfo info, bool includeDiffs = false)
+	private static void RenderCommitInfo(
+		StringBuilder sb,
+		CommitInfo info,
+		bool includeDiffs = false
+	)
 	{
 		sb.Append("commit ");
 		sb.Append(info.Id);
---
+++ core/ExplanationPrompts.cs
@@ -0,0 +1,9 @@
+namespace CalmOnion.GAA;
+
+public static class ExplanationPrompts
+{
+	public const string AsSelf = """
+	Consider the following commit message and diffs,
+	explain what I did as if you were me.
+	""";
+}
